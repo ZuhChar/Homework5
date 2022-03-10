ada9956e3b39d18c70b33ef2ce29e424d6cac0dc
ZuhChar <zcrimmel@uwyo.edu>
2022-03-09 18:25:02 -0700

    f

--- wyls.c
+++ wyls.c
@@ -74,8 +74,8 @@ void _ls(const char *dir, int op_a, int op_l, int op_n, int op_h)
 			userID = geteuid();
 			userName = getlogin();
 			group = getgid();
-			groupName = getgrnam(userName);
-			printf(" %s\t%s\t%s\t%s\n", userName, groupName, userName, d->d_name);
+			groupName.gr_name = getgrnam(userName);
+			printf(" %s\t%s\t%s\t%s\n", userName, groupName.gr_Name, userName, d->d_name);
 		}
 	}
 	if (!op_l)
